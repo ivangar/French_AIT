f3ca30feec6f2497abfee67ff978340144be45b3
Ivan Garzon <garzon861@gmail.com>
2017-03-16 11:33:55 -0400

    Removed accredited program source code
    as well added the copy of the script from the folder
    /lib/php/process_AIT_evaluation.php, I only added line 27 to it.

--- ait.php
+++ ait.php
@@ -1,11 +1,6 @@
 <?php 
-//REQUIRE FIREPHP AND INITIALIZE OBJECT
-require_once($_SERVER['DOCUMENT_ROOT'] . '/FirePHPCore/FirePHP.class.php');
-require_once($_SERVER['DOCUMENT_ROOT'] . '/FirePHPCore/fb.php');
 session_start();
-ob_start(); // Turn on output buffering. From this point output is stored in an internal buffer 
 require_once('../../inc/php/membersite_config.php');
-require_once("../lib/php/program.php"); 
 
 if(isset($_COOKIE['rememberme']) && !empty($_COOKIE['rememberme']) ){
 	$fgmembersite->CheckCookie();
@@ -24,80 +19,10 @@ if(isset($_POST['logout_submitted']))
 	$fgmembersite->LogOut();
 }
 
-
-//Create a Programs instances
-$FrenchProgram = new Program();
-
 //----------------DEFINE PERMANENT VARIABLES FOR THIS PROGRAM-----------------------//
-
-$program_id_fr = 'AIT_01_FR';   //Program ID
-$pretestId = 'AIT_Pre_01';  //program_section_id that belongs to the specific program pretest from program_sections table
-$posttestId = 'AIT_Post_01';  //program_section_id that belongs to the specific program post test from program_sections table
-$forum_id = 'AIT_Forum_01';  //program_section_id that belongs to the specific program forum from program_sections table
-$evaluation_id = 'AIT_Eval_01';  //program evaluation
 $evaluation_id_fr = 'AIT_Eval_01_FR';  //program evaluation
-$certificate_id = 'M_Pro_01';	//Certificate id that belongs to a program
-$certificate_id_fr = 'FMOQ_01_FR';	//Certificate id that belongs to a program
-$topicIds = array("AIT_topic_04", "AIT_topic_05", "AIT_topic_06", "AIT_topic_07", "AIT_topic_08", "AIT_topic_09", "AIT_topic_10", "AIT_topic_11", "AIT_topic_12", "AIT_topic_13");  //Array of forum topic ids for this program
-$sections = array("pretest", "postTest", "forum");  //These are required sections to obtain certificate
-$no_sections = sizeof($sections);
-
-//----------------DEFINE PERMANENT VARIABLES FOR THIS PROGRAM-----------------------//
-
-$program_status = false;		//This will check the program_status field in the doctor profile
-$program_completed = false;	  //This boolean value is used to allow to display the hidden certificate link if all 3 sections are complete
-$sections_status = array();  //This array will hold the state of each program section whenever the page is loaded and reloaded
-$no_sections_completed = 0;
-
-$FrenchProgram->Set_Program($program_id_fr, $pretestId, $posttestId, $forum_id, $evaluation_id_fr, $certificate_id_fr);
-
-//Check that a record having this program ID for this doctor ID exists in doctor_profiles table. 
-//If not Insert a new record with the certificate id defined, program_status = 0, date of completion = NULL and time required Null
-//NOTE: The doctor_profiles record will be updated once each program section is submitted
-
-if(!$FrenchProgram->CheckProfileExists()){
-
-	$FrenchProgram->CreateProfile(); //Create empty French profile
-}
-
-$program_status = $FrenchProgram->CheckProgramStatus();  //First check in the doctor profile to see if the program status is completed.
-$FrenchProgram->GetSectionsStatus();	//Allways get the sections status regardles of program completion.
-$sections_status = $FrenchProgram->sections_status;		//Allways set the section status array (will hold sections status every time user visits program)
-
-if($program_status){
-	$program_completed = true;
-	$no_sections_completed = $no_sections;
-}
-
-//if program has not been completed in the profile check sections one by one everytime the program page is reloaded
-if(!$program_status){
-
-	//if all $no_sections sections are completed update profile with completed
-	if($FrenchProgram->CheckSectionsCompleted()){
-		$FrenchProgram->UpdateProfile($date_of_completion);  //call UpdateProfile to insert program_status = 1, date of completion = NOW()
-		$program_completed = true;
-		$no_sections_completed = $no_sections;
-	}
-}
-
-
-//This will display program progress status
-if($no_sections_completed !== $no_sections){
-    	foreach($sections_status as $section => $status)
-    	{
-    		if((strcmp($section,'evaluation') == 0) || (strcmp($section,'forum') == 0)) break;
-
-    		elseif($status)
-        	$no_sections_completed++;
-    	}
-}
-
 
 if( !isset($_SESSION['arrow_box'])){ $_SESSION['arrow_box'] = true; }
-
-// close connection 
-mysqli_close($FrenchProgram->con);
-
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -209,56 +134,6 @@ line-height: 10px;
 		  <td style="padding: 10px 0 0 0;">
 	          <section id="wrapper" class="wrapper">
 	              <div id="v-nav">
-	              	<!--  THIS LIST WAS USED FOR WHEN THE PROGRAM WAS ACCREDITED
-	              		SEE MODIFIED LIST BELOW!!
-					  <ul>
-	                      <li tab="tab1" class="first current" >Introduction</li>
-	                      <li tab="tab2" > Soutien commercial </li>
-	                      <li tab="tab3" >Pré-test</li>
-	                      <li tab="tab4" >Description de la rhinite allergique<br/><span class="slides_qty">(8 diapositives)</span></li>
-	                      <li tab="tab5" >Immunothérapie sous-cutanée<br/><span class="slides_qty">(10 diapositives)</span></li>
-						  <li tab="tab6" >Comprimé d’immunothérapie sublingual<br/><span class="slides_qty">(30 diapositives)</span></li>
-						  <li tab="tab7" >Approche dans la pratique de médecine familiale<br/><span class="slides_qty">(11 diapositives)</span></li>
-						  <li tab="tab8" >Forum de discussion</li>
-						  <li tab="tab9" >Post-test</li>
-	                      <li tab="tab10" >Questionnaire d'évaluation</li>
-	                      <li tab="tab11" class="last">Agrément</li>
-
-					  </ul>
-					  <div class="tab-content" >
-	                  	<?php //require_once('introduction.html'); ?>  
-	                  </div>
-	                  <div class="tab-content" >
-	                  	<?php //require_once('soutien_commercial.html'); ?>  
-	                  </div>
-	                  <div class="tab-content" >
-	                  		<?php //require_once('pretest.html'); ?>  
-	                  </div>
-	                  <div class="tab-content" id="AR_section">
-	                  		<?php //require_once('AR/AIT_AR_slide_1.html'); ?>
-	                  </div>
-	                  <div class="tab-content" id="SCIT_section">
-	                        <?php //require_once('SCIT/AIT_SCIT_slide_1.html'); ?> 
-	                  </div>
-	                  <div class="tab-content" id="SLIT_section">
-	                        <?php //require_once('SLIT/AIT_SLIT_slide_1.html'); ?>     
-	                  </div>
-	                  <div class="tab-content" id="AFP_section">
-	                  	<?php //require_once('AFP/AIT_AFP_slide_1.html'); ?>           
-	                  </div>
-	                  <div class="tab-content">
-	                      <?php //require_once('forum.php'); ?>           
-	                  </div>
-	                  <div class="tab-content" id="post_test_section" >
-	                  	<?php //require_once('postest.html'); ?> 
-	                  </div>
-	                  <div class="tab-content" >
-	                  	<?php //require_once('evaluation.html'); ?> 
-	                  </div>
-	                  <div class="tab-content" >
-	                  	<?php //require_once('accreditation.html'); ?> 
-	                  </div>
-					  END OF ORIGINAL ACCREDITED LIST-->
 					  <ul>
 	                      <li tab="tab1" class="first current" >Introduction</li>
 	                      <li tab="tab2" > Soutien commercial </li>
@@ -286,11 +161,9 @@ line-height: 10px;
 	                  <div class="tab-content" id="AFP_section">
 	                  	<?php require_once('AFP/AIT_AFP_slide_1.html'); ?>           
 	                  </div>
-
 	                  <div class="tab-content" >
 	                  	<?php require_once('evaluation.html'); ?> 
 	                  </div>
-
 	              </div>
 	          </section>
   	</td>
@@ -319,4 +192,3 @@ var MenuBar1 = new Spry.Widget.MenuBar("MenuBar1");
 </script>
 </body>
 </html>
-<?php ob_flush(); //This function will send the contents of the output buffer ?>
--- js/program.js
+++ js/program.js
@@ -29,17 +29,6 @@
 		return counter;
 	};
 
-	function getCookie(cname) {
-	    var name = cname + "=";
-	    var ca = document.cookie.split(';');
-	    for(var i=0; i<ca.length; i++) {
-	        var c = ca[i];
-	        while (c.charAt(0)==' ') c = c.substring(1);
-	        if (c.indexOf(name) == 0) return c.substring(name.length,c.length);
-	    }
-	    return "";
-	}
-
     /*$.noConflict();*/
     $(document).ready(function () {
     	//if jquery-hashchange-plugin is not used, the following statement will load and reload the tab in the hash url
